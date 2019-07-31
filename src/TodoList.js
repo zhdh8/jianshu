b4a1d551b18b2ce065e6ab5968d967e70b5cd5db
zhdh <1367687498@qq.com>
2019-07-31 15:59:11 +0800

    todolist实现

---
+++ src/TodoList.js
@@ -0,0 +1,55 @@
+import React, { Component, Fragment } from 'react';
+
+class TodoList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      inputVal: '',
+      list: ['learm one', 'learn two']
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="input-wrap">
+          <label htmlFor="todoVal">todo：</label>
+          <input id="todoVal" value={ this.state.inputVal } onChange={ this.handleInputChange.bind(this) } />
+          <button onClick={ this.handleSubmit.bind(this) }>提交</button>
+        </div>
+        <ul>
+          {
+            this.state.list.map((item, index) => {
+              return (
+                // <li key={index} onClick={ this.handleDel.bind(this, index) }>{item}</li>
+                <li key={index} onClick={ _ => { this.handleDel(index) } } dangerouslySetInnerHTML={{__html: item}}></li>
+              )
+            })
+          }
+        </ul>
+      </Fragment>
+    )
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      inputVal: e.target.value
+    })
+  }
+  handleSubmit() {
+    this.setState({
+      list: [...this.state.list, this.state.inputVal],
+      inputVal: ''
+    })
+  }
+  handleDel(index) {
+    let list = [...this.state.list]
+    list.splice(index, 1)
+    this.setState({
+      list: list
+    })
+  }
+}
+
+export default TodoList;
