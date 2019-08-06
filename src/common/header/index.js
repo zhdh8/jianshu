bc1a634116bddda4f8cd92f13863e84dc17acf7e
zhdh <1367687498@qq.com>
2019-08-06 15:56:57 +0800

    整理

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
-import { HeaderWrapper, Contain, Logo } from './style';
+import { CSSTransition } from 'react-transition-group';
+import { HeaderWrapper, Wrapper, Contain, Logo, Nav, NavItem, SearchInput } from './style';
 
 class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isFocused: false
+    }
+
+    this.handleFocus = this.handleFocus.bind(this)
+    this.handleBlur = this.handleBlur.bind(this)
+  }
+
   render() {
     return (
       <HeaderWrapper>
-        <Contain>
+        <Wrapper>
           <Logo></Logo>
-        </Contain>
+          <Contain>
+            <Nav>
+              <NavItem className="active">首页</NavItem>
+              <NavItem>下载</NavItem>
+              <CSSTransition in={this.state.isFocused} timeout={200} classNames='slide'>
+                <NavItem className='searchWrap'>
+                  <SearchInput className={this.state.isFocused?'focused': ''} onFocus={this.handleFocus} onBlur={this.handleBlur}></SearchInput>
+                </NavItem>
+              </CSSTransition>
+            </Nav>
+          </Contain>
+        </Wrapper>
       </HeaderWrapper>
     )
+  }
+
+  handleFocus() {
+    this.setState({
+      isFocused: true
+    })
+  }
+  handleBlur() {
+    this.setState({
+      isFocused: false
+    })
   }
 }
 
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import logoImage from '../../static/logo.png';
 
 export const HeaderWrapper = styled.div`
   height: 58px;
@@ -10,7 +11,7 @@ export const HeaderWrapper = styled.div`
   z-index: 1030;
 `;
 
-export const Contain = styled.div`
+export const Wrapper = styled.div`
   width: 1440px;
   height: 56px;
   light-height: 56px;
@@ -20,5 +21,63 @@ export const Contain = styled.div`
 export const Logo = styled.a.attrs({
   'href': '/'
 })`
+  display: inline-block;
+  width: 100px;
+  height: 56px;
+  background: url(${logoImage});
+  background-size: contain;
+`;
 
+export const Contain = styled.div`
+  display: inline-block;
+  width: 960px;
+  height: 56px;
+  light-height: 56px;
+  margin: 0 auto;
+`;
+
+export const Nav = styled.div`
+  
+`;
+
+export const NavItem = styled.a`
+  display: block;
+  width: 90px;
+  height: 56px;
+  line-height: 56px;
+  margin-right: 10px;
+  text-align: center;
+  float: left;
+  &.active {
+    color: #ea6f5a;
+  }
+  &.searchWrap {
+    .slide-enter {
+      transition: all .2s ease-in;
+    }
+    .slide-enter-active {
+      width: 240px;
+    }
+    .slide-exit: {
+      transition: all .2s ease-out;
+    }
+    .slide-exit-active: {
+      width: 180px;
+    }
+  }
+`;
+
+export const SearchInput = styled.input.attrs({
+  'placeholder': '搜索'
+})`
+  width: 180px;
+  height: 38px;
+  border-radius: 40px;
+  padding: 0 40px 0 20px;
+  border: 1px solid #eee;
+  background: #eee;
+  outline: none;
+  &.focused {
+    width: 255px;
+  }
 `;
